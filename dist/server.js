cc1b8b94834819ec456216b3c2e4c875823b8757
Greyson <greysonf@gmail.com>
2020-12-06 23:33:19 -0300

    update project

--- dist/server.js
+++ dist/server.js
@@ -10,12 +10,13 @@ var _routes = _interopRequireDefault(require("./routes"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-// var port = process.env.PORT || 3333;
+var port = process.env.PORT || 3333;
 const app = (0, _express.default)();
 app.use((0, _cors.default)());
 app.use(_express.default.json());
 app.use(_routes.default);
-app.use('/uploads', _express.default.static(_path.default.resolve(__dirname, '..', 'uploads')));
-app.listen(3333); // app.listen(port, () => {
-//   console.log('Backend Started...')
-// })
+app.use('/uploads', _express.default.static(_path.default.resolve(__dirname, '..', 'uploads'))); // app.listen(3333);
+
+app.listen(port, () => {
+  console.log('Backend Started...');
+});
